b06d4ccc02aa01a28f09e7b66ada60dc4b6113b9
aans268 <161146220+aans268@users.noreply.github.com>
2024-02-24 17:39:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 // Fonction pour masquer toutes les sections sauf celle spécifiée
 function toggleSection(sectionId) {
-    var sections = document.querySelectorAll('main section');
-    sections.forEach(function(section) {
-        if (section.id === sectionId) {
-            if (section.style.display === "none") {
-                section.style.display = "block";
-            } else {
-                section.style.display = "none";
-            }
-        } else {
-            section.style.display = "none";
-        }
-    });
+    var section = document.getElementById(sectionId);
+    if (section.style.display === "none" || section.style.display === "") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
 }
 
-// Fonction pour afficher la section de présentation
+// Fonction pour afficher la section de présentation et masquer les autres
 function showPresentation() {
     var sections = document.querySelectorAll('main section');
     sections.forEach(function(section) {
@@ -25,3 +19,6 @@ function showPresentation() {
         }
     });
 }
+
+// Afficher la page de présentation par défaut lors du chargement du site
+showPresentation();
