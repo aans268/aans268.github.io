a2c47f23cabd3a18b478760464f6c625f334403b
aans268 <161146220+aans268@users.noreply.github.com>
2024-02-24 17:18:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+// Fonction pour masquer toutes les sections sauf celle spécifiée
 function toggleSection(sectionId) {
-    var section = document.getElementById(sectionId);
-    if (section.style.display === "none") {
-        section.style.display = "block";
-    } else {
-        section.style.display = "none";
-    }
+    var sections = document.querySelectorAll('main section');
+    sections.forEach(function(section) {
+        if (section.id === sectionId) {
+            if (section.style.display === "none") {
+                section.style.display = "block";
+            } else {
+                section.style.display = "none";
+            }
+        } else {
+            section.style.display = "none";
+        }
+    });
 }
